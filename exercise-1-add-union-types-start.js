ba745e7e59886dd07334087fd164953bc50c0489
Amelia Tedeschi <tedeschiamelia@gmail.com>
2022-06-09 17:44:11 +0200

    fix js

---
+++ exercise-1-add-union-types-start.js
@@ -0,0 +1,21 @@
+"use strict";
+/**
+ * START: Follow the instructions below.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.narniaCurrency = exports.indianCurrency = void 0;
+let indianCurrency = "Indian rupee";
+exports.indianCurrency = indianCurrency;
+let narniaCurrency = false;
+exports.narniaCurrency = narniaCurrency;
+// Change the type for the `data` function parameter so it can accept
+// the different types of data that are passed when it's being called.
+//
+// This should fix the type error on Lines 28 and 30.
+function outputCountryData(country, data) {
+    console.log(`The data for ${country} is: ${data}`);
+}
+outputCountryData("Italy", 60317116);
+outputCountryData("Nigeria", "Naira");
+outputCountryData("United States of America", "English");
+outputCountryData("India", 1352642280);
---
+++ exercise-1-construct-types-from-types-start.js
@@ -0,0 +1,40 @@
+"use strict";
+/**
+ * START: Follow the instructions below.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.countryE = exports.countryD = exports.countryB = exports.countryA = void 0;
+const countryA = {
+    code: "CN",
+    population: 1412600000,
+};
+exports.countryA = countryA;
+const countryB = {
+    name: "Greece",
+    code: "GR",
+    languages: ["Greek"],
+    currency: "Euro",
+    population: 10678632,
+};
+exports.countryB = countryB;
+const countryC = {
+    name: "Italy",
+    code: "IT",
+    languages: ["Italian"],
+    population: 60317116,
+};
+// countryC.population = 60_317_117;
+console.log(countryC);
+const countryD = {
+    name: "New Zealand",
+    code: "NZ",
+    population: 5135300,
+};
+exports.countryD = countryD;
+const countryE = {
+    name: "Thailand",
+    code: "TH",
+    languages: ["Thai", "Isan", "Kam Mueang", "Pak Tai", "Malay"],
+    currency: "Baht",
+};
+exports.countryE = countryE;
