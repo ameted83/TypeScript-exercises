d43bd357ccbfe3b0df09a1350f224014e12840c7
Amelia Tedeschi <tedeschiamelia@gmail.com>
2022-06-09 17:55:18 +0200

    add js

---
+++ exercise-2-create-intersection-types-start.js
@@ -0,0 +1,18 @@
+"use strict";
+/**
+ * START: Follow the instructions below.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.countryB = exports.countryA = void 0;
+const countryA = {
+    name: "Greece",
+    code: "GR",
+    languages: ["Greek"],
+};
+exports.countryA = countryA;
+const countryB = {
+    name: "China",
+    code: "CN",
+    population: 1412600000,
+};
+exports.countryB = countryB;
